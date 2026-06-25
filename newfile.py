c7bc20238be430ad2f82b85003bcfaa6d187398e
aditya97929937-design <aditya97929937@gmail.com>
2026-06-25 22:49:09 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,23 @@
+const products = [
+{
+id:1,
+name:"Dior Sauvage",
+price:7999,
+category:"Men",
+image:"images/perfume1.jpg"
+},
+{
+id:2,
+name:"Bleu De Chanel",
+price:9499,
+category:"Men",
+image:"images/perfume2.jpg"
+},
+{
+id:3,
+name:"Versace Eros",
+price:6499,
+category:"Men",
+image:"images/perfume3.jpg"
+}
+];
